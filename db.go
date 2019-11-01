386b38e57a3ab56a5676b77c74cc0a3a2d6b4bdb
Zach Taylor <zach@ztaylor.me>
2019-11-01 16:20:05 -0500

    update ztaylor.me/cast@v0.0.4

--- db.go
+++ db.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version is the module version
-const Version = "v0.0.3"
+const Version = "v0.0.4"
 
 // ErrPatchTable is returned by Patch when the patch table doesn't exist
 var ErrPatchTable = errors.New(`table "patch" does not exist`)
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module ztaylor.me/db
 require (
 	github.com/go-sql-driver/mysql v1.4.1
 	google.golang.org/appengine v1.6.2 // indirect
-	ztaylor.me/cast v0.0.3
+	ztaylor.me/cast v0.0.4
 	ztaylor.me/env v0.0.0
 	ztaylor.me/log v0.0.4
 )
